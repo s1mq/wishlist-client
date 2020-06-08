74e1e9bc68ffc7d366153638a288577c64984a25
Siim Tsarents <siim.tsarents@gmail.com>
2020-06-08 22:23:23 +0300

    Add left sidebar for groups and design main client

--- assets/app.js
+++ assets/app.js
@@ -16,6 +16,7 @@ async function doLoadClient() {
     let client = await fetchClient();
     if (client) {
         displayClient(client);
+        openWishlistItem();
     }
 }
 
@@ -31,19 +32,21 @@ function displayClient(client) {
     let clientHtml = '';
 
     clientHtml += /*html*/ `
-        <div class="client-box">
-            <div class="client-photo"><img src="${client.photo}" width="200" height="200"></div>
-            <div class="client-name">${client.name}</div>
-            <div class="client-groups">
-                <h3>Groups</h3>
+        <div id="client-box">
+            <div id="client-header">
+                <div id="client-photo"><img src="${client.photo}"></div>
+                <div id="client-name">${client.name}</div>            
+            </div>            
+            <div id="client-groups-nav">
+                <div>
+                    <i id="nav-button-container" onclick="toggleNavigation()" class="material-icons">view_headline</i>
+                </div>
                 ${displayGroups(client.groups)}            
             </div>
-            <div class="client-dates">
-                <h3>Dates</h3>
+            <div id="client-dates">
                 ${displayDates(client.dates)}
             </div>
-            <div class="client-wishlist">
-                <h3>Wishlist</h3>
+            <div id="client-wishlist">
                 ${displayWishlist(client.wishlistItems)}
             </div>
         </div>
@@ -55,12 +58,13 @@ function displayGroups(groups) {
     let groupsHtml = '';
     for (let group of groups) {
         groupsHtml += /*html*/`
-        <div>
-            <div>${group.id}</duv>
-            <div>${group.name}</duv>
-            <div>${group.picture}</duv>
-            <div>${group.description}</duv>
-            <div>${group.userId}</duv>
+        <div class=client-group">
+            <div class="group-el">
+            <img src="${group.picture}">
+            </div>
+            <div class="group-el">
+                <h2>${group.name}</h2>            
+            </div>            
         </div>
         `;
     }
@@ -71,14 +75,19 @@ function displayDates(dates) {
     let datesHtml = '';
     for (let date of dates) {
         datesHtml += /*html*/ `
-        <div>
-            <div>${date.id}</div>
-            <div>${date.name}</div>
-            <div>${date.date}</div>
-            <div>${date.userId}</div>
+        <div class="client-date">
+            <div class="date-name">${date.name}</div>
+            <div class="date-date">${date.date}</div>
         </div>
         `;
     }
+    datesHtml += /*html*/`
+    <div class="add-element">
+        <div class="add-container">
+            <i class="material-icons">add</i>
+        </div>
+    </div>
+    `;
     return datesHtml;
 }
 
@@ -86,17 +95,85 @@ function displayWishlist(wishlistItems) {
     let wishlistItemsHtml = '';
     for (let item of wishlistItems) {
         wishlistItemsHtml += /*html*/`
-        <div>
-            <div>${item.id}</div>
-            <div>${item.status}</div>
-            <div>${item.itemName}</div>
-            <div>${item.price}</div>
-            <div>${item.picture}</div>
-            <div>${item.description}</div>
-            <div>${item.link}</div>
-            <div>${item.userId}</div>
+        <div class="wishlist-item">
+            <div class="item-name">${item.itemName}</div>            
+            <div class="expand-item">
+                <i class="material-icons">keyboard_arrow_down</i>
+            </div>
+
         </div>
+        <div class="item-content">
+            <div class="item-description">
+                
+                <div class="item-desc-elements">
+                    <img class="item-picture" src="${item.picture}">
+                    <div class="item-desc-text">${item.description}</div> 
+                    <div style="font-size: 2em;">${item.price} €</div>                   
+                </div>
+            </div>
+        </div>        
         `;
     }
+    wishlistItemsHtml += /*html*/`
+    <div class="add-element">
+        <div class="add-container">
+            <i class="material-icons">add</i>
+        </div>
+    </div>
+    `;
     return wishlistItemsHtml;
+}
+
+let isNavigationOpen = false;
+
+function openLeftNavigation() {
+    let navigationElement = document.querySelector('#client-groups-nav');
+    navigationElement.style.width = '50%';
+    isNavigationOpen = true;
+}
+
+function closeLeftNavigation() {
+    let navigationElement = document.querySelector('#client-groups-nav');
+    navigationElement.style.width = '150px';
+    isNavigationOpen = false;
+}
+
+function toggleNavigation() {
+    let menuButtonContainer = document.querySelector('#nav-button-container');
+    if (isNavigationOpen) {
+        menuButtonContainer.innerHTML = '<i class="material-icons">view_headline</i>';
+        closeLeftNavigation();
+    } else {
+        menuButtonContainer.innerHTML = '<i class="material-icons">close</i>';
+        openLeftNavigation();
+    }
+    // isNavigationOpen ? closeLeftNavigation() : openLeftNavigation();
+}
+
+function test() {
+    let myArray = document.querySelectorAll(".wishlist-item");
+    console.log('My array length is: ', myArray)
+}
+
+function openWishlistItem() {
+    let coll = document.querySelectorAll(".wishlist-item");
+    console.log('coll length: ', coll.length);
+    let i;
+    
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            let content = this.nextElementSibling;
+            let expandIcon = document.querySelector(".expand-item");
+            if (content.style.display === "flex") {
+                content.style.display = "none";
+                expandIcon.innerHTML = '<i class="material-icons">keyboard_arrow_down</i>';
+            } else {
+                content.style.display = "flex";
+                expandIcon.innerHTML = '<i class="material-icons">keyboard_arrow_up</i>';
+
+            }
+        });
+    }
 }
